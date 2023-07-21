5374dfccee1ad3609356e719023fd3412baafba3
pantcr7 <prasantpant141@gmail.com>
2023-07-21 11:16:44 +0545

    client script updated

--- customization/public/js/support/support.js
+++ customization/public/js/support/support.js
@@ -1,12 +1,39 @@
 frappe.ui.form.on('Issue', {
 	after_save:function(frm) {
 		// your code here
-		 if (frm.doc.status === 'Closed') { //&& frm.doc.resolution_date === undefined
-            frm.set_value('resolution_date', frappe.datetime.get_today());
-            frm.set_value('resolution_time', frappe.datetime.now_time());
-            frm.save();
-        }
-	},
+		if(frm.doc.status == "Closed"){
+                frm.set_value('resolution_date', frappe.datetime.now_datetime());
+                 frm.set_value('starting_date', frm.doc.opening_date+ " "+ frm.doc.opening_time);
+                var closed_date = frm.doc.resolution_date;
+                var opened_date = frm.doc.starting_date;
+                var new_closedDate = new Date(closed_date);
+                var new_openedOpened = new Date(opened_date);
+       
+                
+                
+                 var timeDifference = new_closedDate.getTime() - new_openedOpened.getTime();
+              
+                 
+                 var days = Math.floor(timeDifference / (1000 * 3600 * 24));
+                 var hours = Math.floor((timeDifference % (1000 * 3600 * 24)) / (1000 *60*60));
+                 var mins = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 *60));
+                 var secs =Math.floor((timeDifference % (1000 * 60 )) / (1000 ));
+
+        // Display the days difference in the console
+                
+            frm.set_value('work_duration_', days + " "+ hours + ":"+mins+":"+secs);
+               // frm.set_value('user_resolution_time', "10 1:20:55");
+               if (!frm._saving) {
+            // Set the flag to indicate that the save process has started
+                    frm._saving = true;
+            // Call the save function
+                    frm.save();
+                }
+                
+                
+           }
+           
+        },
 	refresh:function(frm){
 	    frappe.call({
             method: 'frappe.client.get_value',
@@ -39,12 +66,12 @@ frappe.ui.form.on('Issue', {
             }
         }
         else{
-            for (var fieldname in fields) {
+            for (var fieldname2 in fields) {
             // Set all fields as read-only 0 except for the specific field
-                if(fieldname == 'opening_date' || fieldname == 'opening_time'){
-                   frm.set_df_property(fieldname, 'read_only', 1)}
+                if(fieldname2 == 'opening_date' || fieldname2 == 'opening_time'){
+                   frm.set_df_property(fieldname2, 'read_only', 1)}
                 else{
-                    frm.set_df_property(fieldname, 'read_only', 0)
+                    frm.set_df_property(fieldname2, 'read_only', 0)
                 }
                     
             }
